c18d3dfab00e150817e7bb0805263be818efcb9a
42826705172 <42826705172@CPD-SSI-343560.ad-fub.unb.br>
2017-09-28 07:57:16 -0300

    CORREÇÃO NAS PENDENCIAS APÓS O CURSO.

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -5,17 +5,19 @@ $setPost = array_map('strip_tags', $getPost);
 $Post = array_map('trim', $setPost);
 
 $Action = $Post['action'];
-$jSon = array();
 unset($Post['action']);
+$jSon = array();
 sleep(1);
+
 
 if ($Action):
     require '../_app/Config.inc.php';
-    $Read = new Read;
     $Create = new Create;
+    $Read = new Read;
     $Update = new Update;
     $Delete = new Delete;
 endif;
+
 
 switch ($Action):
     case 'upload':
@@ -23,51 +25,46 @@ switch ($Action):
         $Upload->Image($_FILES['imagem']);
         break;
 
+
     case 'create':
         if (in_array('', $Post)):
-            $jSon['error'] = "<b>OPPSSS:</b> Para Cadastrar Um Usuário. Preencha todos os campos!";
+            $jSon['error'] = "<b>OPSSS:</b>Para Cadastrar um Usuário, preenca todos os campos!";
         elseif (!Check::Email($Post['user_email']) || !filter_var($Post['user_email'], FILTER_VALIDATE_EMAIL)):
-            $jSon['error'] = "<b>OPPSSS:</b> Favor Informe Um E-mail Válido!";
-        elseif (strlen($Post['user_password']) < 5 || strlen($Post['user_password']) > 10):
-            $jSon['error'] = "<b>OPPSSS:</b> Sua Senha deve Ter entre 5 e 10 Caracteres!";
+            $jSon['error'] = "<b>OPSSS:</b>Favor, Informe um e-mail válido!";
+        elseif (strlen($Post['user_password']) < 5 || strlen($Post['user_password']) > 10) :
+            $jSon['error'] = "<b>OPSSS:</b>Sua senha deve ter entre 5 e 10 caracteres!";
         else:
             $Read->FullRead("SELECT user_id FROM ws_users WHERE user_email = :email", "email={$Post['user_email']}");
             if ($Read->getResult()):
-                $jSon['error'] = "<b>OPPSSS:</b> O e-mail <b>{$Post['user_email']}</b> já está em uso!";
+                $jSon['error'] = "<b>OPSSS:</b>O e-mail <b>{$Post['user_email']}</b> já está em uso!";
             else:
                 $Create->ExeCreate('ws_users', $Post);
-                $jSon['success'] = "Cadastro com Sucesso!";
+                $jSon['success'] = "Cadastro com sucesso!";
                 $jSon['result'] = "<article style='display:none' class='user_box j_register' id='{$Create->getResult()}'><h1>{$Post['user_name']} {$Post['user_lastname']}</h1><p>{$Post['user_email']} (Nível {$Post['user_level']})</p><a class='action edit j_edit' rel='{$Create->getResult()}'>Editar</a><a class='action del' rel='{$Create->getResult()}'>Deletar</a></article>";
             endif;
         endif;
         break;
 
-
     case 'update':
         if (in_array('', $Post)):
-            $jSon['error'] = "<b>OPPSSS:</b> Para Cadastrar Um Usuário. Preencha todos os campos!";
+            $jSon['error'] = "<b>OPSSS:</b>Para Alterar um Usuário, preenca todos os campos!";
         elseif (!Check::Email($Post['user_email']) || !filter_var($Post['user_email'], FILTER_VALIDATE_EMAIL)):
-            $jSon['error'] = "<b>OPPSSS:</b> Favor Informe Um E-mail Válido!";
-        elseif (strlen($Post['user_password']) < 5 || strlen($Post['user_password']) > 10):
-            $jSon['error'] = "<b>OPPSSS:</b> Sua Senha deve Ter entre 5 e 10 Caracteres!";
+            $jSon['error'] = "<b>OPSSS:</b>Favor, Informe um e-mail válido!";
+        elseif (strlen($Post['user_password']) < 5 || strlen($Post['user_password']) > 10) :
+            $jSon['error'] = "<b>OPSSS:</b>Sua senha deve ter entre 5 e 10 caracteres!";
         else:
             $Read->FullRead("SELECT user_id FROM ws_users WHERE user_email = :email AND user_id != :id", "email={$Post['user_email']}&id={$Post['user_id']}");
             if ($Read->getResult()):
-                $jSon['error'] = "<b>OPPSSS:</b> O e-mail <b>{$Post['user_email']}</b> já está em uso!";
+                $jSon['error'] = "<b>OPSSS:</b>O e-mail <b>{$Post['user_email']}</b> já está em uso!";
             else:
                 $UserId = $Post['user_id'];
                 unset($Post['user_id']);
-                $Update->ExeUpdate('ws_users', $Post, "WHERE user_id = :id", "id={$UserId}");
-                $jSon['success'] = "Usuário atualizado Com Scesso!";
+                $Update->ExeUpdate("ws_users", $Post, "WHERE user_id = :id", "id={$UserId}");
+                $jSon['success'] = "Usuário atualizado com sucesso!";
+                
+                $jSon['result'] = "<article style='display:none' class='user_box j_register' id='{$Create->getResult()}'><h1>{$Post['user_name']} {$Post['user_lastname']}</h1><p>{$Post['user_email']} (Nível {$Post['user_level']})</p><a class='action edit j_edit' rel='{$Create->getResult()}'>Editar</a><a class='action del' rel='{$Create->getResult()}'>Deletar</a></article>";
             endif;
         endif;
-        break;
-
-    case 'deleteuser':
-        $Delete->ExeDelete('ws_users', "WHERE user_id = :id", "id={$Post['user_id']}");
-        if (!$Delete->getRowCount()) {
-            $jSon['error'] = true;
-        }
         break;
 
     case 'loadmore':
@@ -83,6 +80,19 @@ switch ($Action):
         endif;
         break;
 
+    case 'deleteuser':
+        // antes deletar verificar se o usário é administrador
+        $Read->ExeRead('ws_users', "WHERE user_id = :id and user_level = 3", "id={$Post['user_id']}");
+        if ($Read->getResult()):
+            $jSon['admin'] = true;
+        else:
+            $Delete->ExeDelete('ws_users', "WHERE user_id = :id", "id={$Post['user_id']}");
+            if (!$Delete->getRowCount()):
+                $jSon['error'] = true;
+            endif;
+        endif;
+
+        break;
 
     case 'readuser':
         $Read->ExeRead('ws_users', "WHERE user_id = :id", "id={$Post['user_id']}");
@@ -93,8 +103,12 @@ switch ($Action):
         endif;
         break;
 
-    default :
-        $jSon['error'] = "Erro ao Selecionar Ação!";
+    default:
+        $jSon['error'] = "Erro ao Selecionar Ação";
+
 endswitch;
 
 echo json_encode($jSon);
+
+
+
